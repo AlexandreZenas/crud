267f105a2ed8a77bd3e02b92f28c23178be07a52
alexandre zenas dos santos ribeiro <alexandrezenas11@gmail.com>
2024-01-26 17:48:41 -0300

    subindo o crud

---
+++ CRUD-React/src/Crud.jsx
@@ -0,0 +1,12 @@
+import './app.css'
+
+export default function Crud(){
+    return(
+        <>
+            <form action="">
+                <input type="text" name="" id="" />
+                <button type='submit'>Enviar</button>
+            </form>
+        </>
+    )
+}
